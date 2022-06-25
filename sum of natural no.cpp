de2b8347b56365423e45927b04a0ddec9fd19682
Prakash Thakur <89472789+CodingThe@users.noreply.github.com>
2022-06-25 13:54:22 +0530

    Add files via upload

---
+++ sum of natural no.cpp
@@ -0,0 +1,18 @@
+#include"iostream"
+using namespace std;
+
+int main(){
+	int i,n;
+	cout<<"Enter a positive number: "<<endl;
+	cin>>n;
+		int sum1= (n*(n+1))/2;
+		cout<<"return "<<sum1<<endl;
+		int sum=0;
+		for(i=0;i<=n;i++){
+			sum=sum+i;
+
+			
+		};
+			cout<<"The sum of natural numbers is :"<<sum<<endl;	
+
+}
